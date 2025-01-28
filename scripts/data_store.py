e64de20bdf6eeaa75bbee26772df6394a1960bf2
Aniket Narkhede <aniketn@Anikets-Mac-mini.local>
2025-01-27 21:42:57 -0500

    created teams_dict

--- scripts/data_store.py
+++ scripts/data_store.py
@@ -2,10 +2,6 @@ import kagglehub
 import pandas as pd
 
 
-
-
-
-
 def generate_fixture_summary():
     return False
 
@@ -13,8 +9,10 @@ def main():
     # load data and create mappings
     path = kagglehub.dataset_download("excel4soccer/espn-soccer-data")
     teams_data_file = f"{path}/base_data/teams.csv"
-    df = pd.read_csv(teams_data_file)
-    print(df.head())
+    teams_df = pd.read_csv(teams_data_file)
+    print(teams_df.head())
+    teams_dict = dict(zip(teams_df['teamId'], teams_df['name']))
+    print(teams_dict)
 
 if __name__ == "__main__":
     main()
